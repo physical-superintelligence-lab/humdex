5f923c61814f286102a52110bed304c57ecb9ee9
BigXu <bigxu@BigXudeMacBook-Air.local>
2026-03-07 20:43:29 +0800

    fix: infer script

--- wuji_hand_real.sh
+++ wuji_hand_real.sh
@@ -1,30 +1,21 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR=$(dirname $(realpath $0))
+cd deploy_real
 
 # Runtime configuration
 redis_ip="localhost"
 hand_side="left"  # "left" or "right"
 target_fps=50
 serial_number="3555374E3533"
-# model inference options
-use_model=true
 policy_tag="wuji_left_new_1"
 policy_epoch=200
 
 # Start controller
-CMD=(
-  python3 "${SCRIPT_DIR}/wuji_policy/server_wuji_hand_redis.py"
-    --hand_side "${hand_side}"
-    --serial_number "${serial_number}"
-    --redis_ip "${redis_ip}"
-    --target_fps "${target_fps}"
-    --no_smooth
-)
-
-if [[ "${use_model}" == "true" ]]; then
-  CMD+=(--use_model --policy_tag "${policy_tag}" --policy_epoch "${policy_epoch}")
-fi
-
-"${CMD[@]}"
+python ../wuji_policy/server_wuji_hand_redis.py \
+    --hand_side ${hand_side} \
+    --serial_number ${serial_number} \
+    --redis_ip ${redis_ip} \
+    --target_fps ${target_fps} \
+    --no_smooth \
+    --use_model \
+    --policy_tag ${policy_tag} \
+    --policy_epoch ${policy_epoch}
--- wuji_hand_sim.sh
+++ wuji_hand_sim.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR=$(dirname $(realpath $0))
+cd deploy_real
 
 # Runtime configuration
 redis_ip="localhost"
@@ -9,23 +7,16 @@ hand_side="left"  # "left" or "right"
 target_fps=50
 glove="manus"      # "manus" or "vdhand"
 retarget_config="${SCRIPT_DIR}/wuji-retargeting/example/config/retarget_${glove}_${hand_side}.yaml"
-# model inference options
-use_model=true
-policy_tag="wuji_left_new_1"
-policy_epoch=200
+policy_tag="geort_filter_wuji"
+policy_epoch=-1
 
 # Start controller
-CMD=(
-  python3 "${SCRIPT_DIR}/deploy_real/server_wuji_hand_sim_redis.py"
-    --hand_side "${hand_side}"
-    --config "${retarget_config}"
-    --redis_ip "${redis_ip}"
-    --target_fps "${target_fps}"
-    --no_smooth
-)
-
-if [[ "${use_model}" == "true" ]]; then
-  CMD+=(--use_model --policy_tag "${policy_tag}" --policy_epoch "${policy_epoch}")
-fi
-
-"${CMD[@]}"
+python server_wuji_hand_sim_redis.py \
+    --hand_side ${hand_side} \
+    --config ${retarget_config} \
+    --redis_ip ${redis_ip} \
+    --target_fps ${target_fps} \
+    --no_smooth \
+    --use_model \
+    --policy_tag ${policy_tag} \
+    --policy_epoch ${policy_epoch} \
