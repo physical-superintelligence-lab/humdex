c9d69c12ef1dc0cf4466e6bdb80f774cd4f97138
Jiajun Xu <143870170+GeorginHsu@users.noreply.github.com>
2026-03-15 13:30:41 +0800

    Update paths in wuji_data_collect.sh script

--- scripts/wuji_data_collect.sh
+++ scripts/wuji_data_collect.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname $(realpath $0))
-cd "${SCRIPT_DIR}/deploy_real"
+cd "${SCRIPT_DIR}/../deploy_real"
 
 # Runtime configuration
 session_dir_name="right_hand_000"   # folder name under deploy_real/humdex_demonstration
 hand_side="right"                   # "left" / "right" / "both"
 output_name="wuji_right_000"
-output_dir="${SCRIPT_DIR}/wuji_policy/data"
-intermediate_root="${SCRIPT_DIR}/deploy_real/wuji_hand_policy_dataset_tmp/${output_name}_${hand_side}"
+output_dir="${SCRIPT_DIR}/../wuji_policy/data"
+intermediate_root="${SCRIPT_DIR}/../deploy_real/wuji_hand_policy_dataset_tmp/${output_name}_${hand_side}"
 
 python ../deploy_real/build_wuji_hand_policy_data.py \
     --session_dir_name ${session_dir_name} \
